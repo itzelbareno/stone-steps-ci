84fb6a041e56019e619a2411b8aae065902fdcf2
itzelbareno <itzelbareno@gmail.com>
2016-06-03 00:03:27 -0700

    Evitar que usuarios entren a páginas sin loggearse
    Agregué ifs en el template para que usuarios que ya iniciaron sesión no
    se vayan a login o signup y al revés, usuarios qu eno están loggeados
    lleguen a otras partes del sitio.

--- application/views/structure/template.php
+++ application/views/structure/template.php
@@ -1,4 +1,12 @@
 <?php
+	if($content != 'signup' && $content != 'login' && !isset($_SESSION['user'])){
+		redirect(base_url().'login');
+	}
+
+	if(($content == 'signup' || $content == 'login') && isset($_SESSION['user'])){
+		redirect(base_url());
+	}
+
 	$this->load->view('structure/header');
     $this->load->view($content); 
     $this->load->view('structure/footer');
